bc872351c74301fa4ca151015eda96511f766451
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-11-27 21:55:16 +0100

    Create logger.js, membersroute.js, Members.js files, then use the GET method to serve a json data.

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [{
+    id: 1, 
+    name: 'Bisi',
+    email: 'Bisi@gmail.com',
+
+},
+{
+    id: 2, 
+    name: 'Ola',
+    email: 'Ola@gmail.com',
+
+
+},
+
+{
+    id: 3, 
+    name: 'Bose',
+    email: 'Bose@gmail.com',
+
+},
+]
+
+module.exports = members;
--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 
-const path = require('path');
-const express = require ('express');
-const app = express();
-require("dotenv").config();
-const dotenv = require("dotenv").config();
 
+const express = require ('express');
+const logger = require('./middleware/logger');
+const path = require('path');
+const app = express();
+// require("dotenv").config();
+// const dotenv = require("dotenv").config();
+
+
+// init middleware
+// app.use(logger);
+
+
+
+// Gets all members
+// app.get('/api/members', (req, res) => 
+//     res.json(members));
+
+// // Get a single member
+// app.get('/api/members/:id', (req, res) => {
+//     const found = members.some(member => member.id === parseInt(req.params.id));
+
+//     if (found) {
+//         res.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+//     } else{
+//         res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+//     }
+    
+// });
 
 // set up static files to serve html file by using a middleware
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api route
+app.use('/api/members', require('./routes/api/membersroute'))
+
+// module.exports = members;
+
+// Create members API
+// app.use('./api/members', require('./routes/api/members'));
+
+// serve json data
+
+
+
 
 
 
@@ -31,7 +68,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/contact', (req, res) => res.sendFile('Hello World'));
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`My first express app listening at PORT ${PORT}` )
 })
