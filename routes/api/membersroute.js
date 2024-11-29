6ab503888f5afdf30ce0be4ad1cc9f83fb46c242
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-11-29 18:50:11 +0100

    Add a DELETE request to the membersroute.js file to delete a member

--- routes/api/membersroute.js
+++ routes/api/membersroute.js
@@ -63,9 +63,17 @@ router.put('/:id', (req, res) => {
 
 
 // delete a member
-// router.get('/:id', (req, res) => 
-//     res.json(members));
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
 
+    if (found) {
+        res.json({msg: 'member deleted', members: members.filter(member => member.id !== parseInt(req.params.id))});
+
+    } else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
 
 
 
