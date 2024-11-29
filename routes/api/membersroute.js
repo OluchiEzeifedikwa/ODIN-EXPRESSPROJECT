7a80434e9994246fa835102c1d1966ef93345903
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-11-29 18:14:59 +0100

    Add a PUT request in membersroute.js file to update a member

--- routes/api/membersroute.js
+++ routes/api/membersroute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../members');
+const uuid = require('uuid');
 
 
 
@@ -20,5 +21,52 @@ router.get('/:id', (req, res) => {
     }
     
 });
+//
+
+// Create or Post a new member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({msg: 'Pls include a name or email'});
+    }
+    members.push(newMember);
+    res.json(members);
+
+
+});
+
+// Update a new member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updMember =  req.body;
+        members.forEach(member => {
+            if(member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email? updMember.email : member.email;
+
+                res.json({msg: 'Member updated', member});
+            };
+        });
+        } else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
+
+// delete a member
+// router.get('/:id', (req, res) => 
+//     res.json(members));
+
+
+
 
 module.exports = router;
